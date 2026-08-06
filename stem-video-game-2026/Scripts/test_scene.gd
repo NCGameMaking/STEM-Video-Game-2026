7852fc7e8b118f0013e57c12a2bea6527d9c2026
NCGameMaking <neil726345@gmail.com>
2026-08-07 07:36:32 +1000

    Imported Environment Assets adjusted atmosphere
    Added 2 relics and a sea mine into the test/water example scene and coded the atmosphere to become darker and darker the further down the submarine goes.

---
+++ stem-video-game-2026/Scripts/test_scene.gd
@@ -0,0 +1,42 @@
+extends Node3D
+
+@export var submarine:RigidBody3D
+@export var world_environment:WorldEnvironment
+@export var sunlight:DirectionalLight3D
+
+const surface_depth = 3
+const zone_2_depth = 100.0
+const zone_3_depth = 200.0
+
+@warning_ignore("unused_parameter")
+func _process(delta):
+	if not submarine or not world_environment or not world_environment.environment:
+		return
+	var depth = abs(submarine.global_position.y)
+	var env = world_environment.environment
+	if submarine.global_position.y >= -surface_depth:
+		if sunlight:
+			sunlight.light_energy = 2.0
+		env.volumetric_fog_enabled = false
+		env.volumetric_fog_density = 0.0
+		env.volumetric_fog_albedo = Color("#ffffff")
+	if depth >= surface_depth and depth < zone_2_depth:
+		env.volumetric_fog_enabled = true
+		var t = (depth - surface_depth) / (zone_2_depth - surface_depth)
+		if sunlight:
+			sunlight.light_energy = lerp(1.8,0.2,t)
+		env.volumetric_fog_density = lerp(0.015,0.04,t)
+		env.volumetric_fog_albedo = lerp(Color("#1a6b72"), Color("#0b2545"), t)
+	elif depth >= zone_2_depth and depth < zone_3_depth:
+		var t = (depth-zone_2_depth) / (zone_3_depth - zone_2_depth)
+		if sunlight:
+			sunlight.light_energy = 0.0
+		env.volumetric_fog_density = lerp(0.01,0.04,t)
+		env.volumetric_fog_albedo = lerp(Color("#0b2545"), Color("#020813"), t)
+		
+	else:
+		if sunlight:
+			sunlight.light_energy = 0.0
+			
+		env.volumetric_fog_density  = 0.9
+		env.volumetric_fog_albedo = Color("#020813")
