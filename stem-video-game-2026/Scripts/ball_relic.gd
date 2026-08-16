4f7509943535be1909aef502a90fdb13822e14c7
NCGameMaking <neil726345@gmail.com>
2026-08-16 11:14:09 +1000

    Made the relics, a shield and orb relic and imported shark
    Made a hovering animation, but planning to remove. Yet to code shark

--- stem-video-game-2026/Scripts/ball_relic.gd
+++ stem-video-game-2026/Scripts/ball_relic.gd
@@ -1,6 +1,18 @@
 extends Area3D
 
+@onready var mms_box_highlight = $MMSBoxHighlight
 
 func _on_body_entered(body):
 	if body.is_in_group("player") or body.name == "Submarine":
+		
 		queue_free()
+
+func trigger_mms_scan() -> void:
+	if mms_box_highlight:
+		mms_box_highlight.visible = true
+		var tween = create_tween()
+		tween.tween_property(mms_box_highlight, "scale", Vector3(1.2,1.2,1.2),0.2)
+		tween.tween_property(mms_box_highlight, "scale", Vector3(1.0,1.0,1.0),0.3)
+		
+		await get_tree().create_timer(2).timeout
+		mms_box_highlight.visible = false
